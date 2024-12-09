f8b63e308052efbd1dd2cefd35b23ca7a31b7997
moamenato <moamenato.tr@gmail.com>
2024-12-09 15:20:54 +0200

    creating APIs and schema

---
+++ MongoDB Schema/category_schema.js
@@ -0,0 +1,20 @@
+// category_schema.js
+const mongoose = require("mongoose");
+
+const category_schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Category", category_schema);
---
+++ app.js
@@ -0,0 +1,27 @@
+// app.js
+const PORT = 8000;
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./APIs/user_api");
+const itemRoutes = require("./APIs/item_api");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const mongouri = "mongodb://localhost:27017/TestWork";
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(mongouri)
+  .then(() => {
+    console.log("connected to MongoDB");
+    app.listen(PORT, () => console.log("app started on port", PORT));
+  })
+  .catch((error) => {
+    console.log("can't connect to mongodb: " + error);
+  });
+
+app.use("/users", userRoutes);
+app.use("/items", itemRoutes);
