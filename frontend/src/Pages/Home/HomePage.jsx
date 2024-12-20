7d471860968b2590eb5f361a55809c37a64b5680
Ahmed Youssef <ahmedyoussef20202@gmail.com>
2024-12-20 03:39:11 +0200

    update home page

--- frontend/src/Pages/Home/HomePage.jsx
+++ frontend/src/Pages/Home/HomePage.jsx
@@ -1,64 +1,42 @@
 import "../../Styles/HomePage.css";
-import Header from "../../Components/Header/Header";
-import NavBar from "../../Components/Header/NavBar";
 import BackToTopButton from "../../Components/BackToTopButton";
 import ImageSlider from "../../Components/ImageSlider/ImageSlider";
 import SectionHeader from "../../Components/SectionHeader/SectionHeader";
 import ProductSwiper from "../../Components/ProductSwiper/ProductSwiper";
-import { mockProductData } from "../../Data/Products"; // Importing the mock data
-
+import image1 from "../../Assets/image1.jpg";
+import image2 from "../../Assets/image2.jpg";
+import image3 from "../../Assets/image3.jpg";
+import React, { useState, useEffect } from "react";
 export default function HomePage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch the products from the API
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/products/");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
   // Mock landing images, later will apply api
-  const images = [
-    "https://via.placeholder.com/1920x1080/FF5733/FFFFFF?text=Image+1",
-    "https://via.placeholder.com/1920x1080/33C1FF/FFFFFF?text=Image+2",
-    "https://via.placeholder.com/1920x1080/FFC933/FFFFFF?text=Image+3",
-  ];
+  const images = [image1, image2, image3];
 
   return (
     <div className="home">
-      <Header />
-
-      <NavBar />
-
       <BackToTopButton />
 
       <ImageSlider images={images} />
 
-      <div className="container">
-        <div className="ad">
-          <img
-            src="https://via.placeholder.com/1200x400/000000/FFFFFF?text=Ad "
-            alt="ad"
-            className="ad"
-          />
-        </div>
-      </div>
-
-      {/* <div className="container">
-        <SectionHeader
-          heading="Why buy from us?"
-          description="With more than 10 years of experience in computer components, which helps us to meet your needs in purchasing the best components at the lowest prices."
-        />
-      </div> */}
-
       <div className="featured-products">
         <div className="container">
-          <SectionHeader
-            heading="Featured Products"
-            description="CHECK LATEST PRODUCTS FROM FEATURES CATEGORIES..."
-          />
-          <ProductSwiper products={mockProductData} />
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="ad">
-          <img
-            src="https://via.placeholder.com/1200x400/000000/FFFFFF?text=Ad "
-            alt="ad"
-            className="ad"
-          />
+          <SectionHeader heading="Featured Products" />
+          <ProductSwiper products={products} />
         </div>
       </div>
     </div>
